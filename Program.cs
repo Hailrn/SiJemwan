20dfb0fd58f5f1eeffd68cfb3150da8f04ef47a3
Wildan <xwildan07@gmail.com>
2025-04-01 20:00:39 +0700

    udah kelar bang

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SiJemwan
+﻿using System.Globalization;
+
+namespace SiJemwan
 {
     class Karyawan
     {
@@ -61,16 +63,16 @@
         {
             return gaji_pokok;
         }
-        public virtual double gaji()
+        public virtual double HitungGaji()
         {
             return gaji_pokok;
         }
         public void info()
         {
-            Console.WriteLine("Nama: " + getNama());
+            Console.WriteLine("\nNama: " + getNama());
             Console.WriteLine("ID: " + getId());
-            Console.WriteLine("Gaji Pokok: " + getGajiPokok());
-            Console.WriteLine("Gaji Akhir: " + gaji());
+            Console.WriteLine("Gaji Pokok: Rp." + getGajiPokok().ToString("N2", new CultureInfo("id-ID")));
+            Console.WriteLine("Gaji Akhir: Rp." + HitungGaji().ToString("N2", new CultureInfo("id-ID")));
         }
     }
     class Tetap : Karyawan
@@ -79,7 +81,7 @@
         public Tetap(string nama, string id, double gaji_pokok) : base(nama, id, gaji_pokok)
         {
         }
-        public override double gaji()
+        public override double HitungGaji()
         {
             return getGajiPokok() + tunjangan;
         }
@@ -91,7 +93,7 @@
         public Kontrak(string nama, string id, double gaji_pokok) : base(nama, id, gaji_pokok)
         {
         }
-        public override double gaji()
+        public override double HitungGaji()
         {
             return getGajiPokok() - potongan_kontrak;
         }
@@ -101,18 +103,95 @@
         public Magang(string nama, string id, double gaji_pokok) : base(nama, id, gaji_pokok)
         {
         }
+        public override double HitungGaji()
+        {
+            return getGajiPokok();
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Kontrak karyawan1 = new Kontrak("Budi", "K001", 3000000);
-            Magang karyawan2 = new Magang("Andi", "M001", 2000000);
-            Tetap karyawan3 = new Tetap("Caca", "T001", 4000000);
-            karyawan3.setGajiPokok(5000000);
-            karyawan1.info();
-            karyawan2.info();
-            karyawan3.info();
+            //Kontrak karyawan1 = new Kontrak("Budi", "K001", 3000000);
+            //Magang karyawan2 = new Magang("Andi", "M001", 2000000);
+            //Tetap karyawan3 = new Tetap("Caca", "T001", 4000000);
+            //karyawan3.setGajiPokok(5000000);
+            //karyawan1.info();
+            //karyawan2.info();
+            //karyawan3.info();
+            string nama, id_pegawai;
+            double gaji_pokok;
+            while (true)
+            {
+                Console.Write("Masukkan Nama Pegawai: ");
+                nama = Console.ReadLine() ?? "";
+
+                if (string.IsNullOrWhiteSpace(nama))
+                {
+                    Console.WriteLine("\n\nNama tidak boleh kosong!!");
+                    continue;
+                }
+
+                break;
+            }
+
+            while (true)
+            {
+                Console.Write("Masukkan ID Pegawai: ");
+                id_pegawai = Console.ReadLine() ?? "";
+
+                if (string.IsNullOrWhiteSpace(id_pegawai))
+                {
+                    Console.WriteLine("\n\nId Pegawai tidak boleh kosong!!");
+                    continue;
+                }
+
+                break;
+            }
+
+            while (true)
+            {
+                Console.Write("Masukkan Gaji Pokok Pegawai: ");
+                string gaji = Console.ReadLine() ?? "";
+
+                if (double.TryParse(gaji, out double Gaji))
+                {
+                    gaji_pokok = Gaji;
+                    break;
+                }
+                Console.WriteLine("\n\nInput Gaji hanya boleh berupa angka!!");
+                continue;
+            }
+
+            while (true)
+            {
+                Console.Write("\n1.Tetap\n2.Kontrak\n3.Magang\nPilih Jenis Pegawai (1/2/3): ");
+                string tipe_pegawai = Console.ReadLine() ?? "";
+
+                if (tipe_pegawai == "1")
+                {
+                    Tetap karyawan = new Tetap(nama, id_pegawai, gaji_pokok);
+                    karyawan.info();
+                    break;
+                }
+                else if (tipe_pegawai == "2")
+                {
+                    Kontrak karyawan = new Kontrak(nama, id_pegawai, gaji_pokok);
+                    karyawan.info();
+                    break;
+                }
+                else if (tipe_pegawai == "3")
+                {
+                    Magang karyawan = new Magang(nama, id_pegawai, gaji_pokok);
+                    karyawan.info();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\n\nInput tidak valid!!");
+                    continue;
+                }
+            }
         }
     }
 }
